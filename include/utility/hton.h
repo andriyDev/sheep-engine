50c199e7b3d3d7fe0e83a56dc3cd2e630c01931d
Andriy Dzikh <andriydzikh@gmail.com>
2021-06-11 15:10:49 -0400

    Refactor hton, ntoh functions into htob, htol, ltoh, btoh.
    Previously, we had network-to-host byte order conversion, but this
    depends on agreement on what network byte order is. Now you just specify
    what byte order you want to target (l)ittle or (b)ig.

--- include/utility/hton.h
+++ include/utility/hton.h
@@ -15,41 +15,73 @@ enum class endian {
 #endif
 };
 
-inline uint32_t htonl(uint32_t value) {
+inline uint32_t swap_bytes(uint32_t value) {
+  union {
+    uint8_t bytes[4];
+    uint32_t host_value;
+  };
+  host_value = value;
+  uint8_t swap = bytes[0];
+  bytes[0] = bytes[3];
+  bytes[3] = swap;
+  swap = bytes[1];
+  bytes[1] = bytes[2];
+  bytes[2] = swap;
+  return host_value;
+}
+
+inline uint16_t swap_bytes(uint16_t value) {
+  union {
+    uint8_t bytes[2];
+    uint16_t host_value;
+  };
+  host_value = value;
+  uint8_t swap = bytes[0];
+  bytes[0] = bytes[1];
+  bytes[1] = swap;
+  return host_value;
+}
+
+// htob, btoh -> Host to big endian
+
+inline uint32_t htobl(uint32_t value) {
   if constexpr (endian::native == endian::big) {
     return value;
   } else {
-    union {
-      uint8_t bytes[4];
-      uint32_t host_value;
-    };
-    host_value = value;
-    uint8_t swap = bytes[0];
-    bytes[0] = bytes[3];
-    bytes[3] = swap;
-    swap = bytes[1];
-    bytes[1] = bytes[2];
-    bytes[2] = swap;
-    return host_value;
+    return swap_bytes(value);
   }
 }
 
-inline uint16_t htons(uint16_t value) {
+inline uint16_t htobs(uint16_t value) {
   if constexpr (endian::native == endian::big) {
     return value;
   } else {
-    union {
-      uint8_t bytes[2];
-      uint16_t host_value;
-    };
-    host_value = value;
-    uint8_t swap = bytes[0];
-    bytes[0] = bytes[1];
-    bytes[1] = swap;
-    return host_value;
+    return swap_bytes(value);
   }
 }
 
-inline uint32_t ntohl(uint32_t value) { return htonl(value); }
+inline uint32_t btohl(uint32_t value) { return htobl(value); }
 
-inline uint16_t ntohs(uint16_t value) { return htons(value); }
+inline uint16_t btohs(uint16_t value) { return htobs(value); }
+
+// ltoh, htol -> Host to little endian
+
+inline uint32_t htoll(uint32_t value) {
+  if constexpr (endian::native == endian::little) {
+    return value;
+  } else {
+    return swap_bytes(value);
+  }
+}
+
+inline uint16_t htols(uint16_t value) {
+  if constexpr (endian::native == endian::little) {
+    return value;
+  } else {
+    return swap_bytes(value);
+  }
+}
+
+inline uint32_t ltohl(uint32_t value) { return htoll(value); }
+
+inline uint16_t ltohs(uint16_t value) { return htols(value); }
